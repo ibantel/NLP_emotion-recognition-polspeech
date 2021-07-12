3f738d6c2af1f0ec3601aed9f871820ed42b8d6f
MatthiasRo <roesti.matthias@gmail.com>
2021-07-12 12:02:24 +0200

    Merging repositories

---
+++ Codes/_Ivo_LSTM_test.py
@@ -0,0 +1,13 @@
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset but only keep the top n words, zero the rest
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
